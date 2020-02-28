21fb35ecb72aab45a36f06a6b67a868e5aa962de
daenur <daenur@outlook.com>
2020-02-28 10:53:38 +0200

    Update checkpoint.ps1

--- checkpoint.ps1
+++ checkpoint.ps1
@@ -1,30 +1,31 @@
-﻿$VM_MASK = "auto-*" # VM name mask
-$VM_SNAPSHOT_COUNT = 5 # minimum count
-$VM_SNAPSHOT_NAME = "Auto" #name prefix
+$VM_MASK = "auto-*" # VM name mask
+$VM_SNAPSHOT_COUNT = 4 # minimum count
+$VM_SNAPSHOT_NAME_PREFIX = "Auto" #name prefix
 
 # stop vm by mask
-Get-VM $VM_MASK | Stop-VM -Force -AsJob
-Get-Job | Wait-Job
+#Get-VM $VM_MASK | Stop-VM -Force -AsJob
+#Get-Job | Wait-Job
 
 
 # take snapshots
-Get-VM $VM_MASK | Checkpoint-VM -SnapshotName "$VM_SNAPSHOT_NAME [$((Get-Date).toshortdatestring()) $((Get-Date).toshorttimestring())]" -AsJob
+Get-VM $VM_MASK | Checkpoint-VM -SnapshotName "$VM_SNAPSHOT_NAME_PREFIX [$((Get-Date).ToShortDateString()) $((Get-Date).ToLongTimeString())]" -AsJob
 Get-Job | Wait-Job
 
 $vm_arr = Get-VM -Name $VM_MASK
 
 foreach($vm in $vm_arr)
 {
-  $snapshot_arr = Get-VMSnapshot ($vm);
-  if ( $VM_SNAPSHOT_COUNT -le $snapshot_arr.Length )
+  $snapshot_arr = $vm | Get-VMSnapshot | Where-Object Name -like "$VM_SNAPSHOT_NAME_PREFIX *"
+  $i = 0
+  while ( $VM_SNAPSHOT_COUNT -le $snapshot_arr.Length-$i )
   {
-    Remove-VMSnapshot -VMSnapshot ($snapshot_arr[0])
+    Remove-VMSnapshot -VMSnapshot ($snapshot_arr[$i++])
   }
 }
 
 # start 
-Get-VM $VM_MASK | Start-VM -AsJob
-Get-Job | Wait-Job
+#Get-VM $VM_MASK | Start-VM -AsJob
+#Get-Job | Wait-Job
 
 # remove completed jobs
 Get-Job -State "Completed" | Remove-Job
