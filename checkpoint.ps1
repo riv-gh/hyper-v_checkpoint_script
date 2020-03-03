c50a8188699776189191ba51770a4f7d1743581d
daenur <daenur@outlook.com>
2020-03-03 14:12:59 +0200

    Update checkpoint.ps1

--- checkpoint.ps1
+++ checkpoint.ps1
@@ -1,17 +1,16 @@
-$VM_MASK = "auto-*" # VM name mask
+$VM_MASK = "test-*" # VM name mask
 $VM_SNAPSHOT_COUNT = 4 # minimum count
-$VM_SNAPSHOT_NAME_PREFIX = "Auto" #name prefix
+$VM_SNAPSHOT_NAME_PREFIX = "Daily" #name prefix
+$TELEGRAM_BOT_TOKEN = "111111111:AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" #INPUT your telegram bot token
+$TELEGRAM_CHAT_ID = "-111111111" #INPUT your telegram chat id (use "-" for group)
 
-# stop vm by mask
-#Get-VM $VM_MASK | Stop-VM -Force -AsJob
-#Get-Job | Wait-Job
-
-
-# take snapshots
-Get-VM $VM_MASK | Checkpoint-VM -SnapshotName "$VM_SNAPSHOT_NAME_PREFIX [$((Get-Date).ToShortDateString()) $((Get-Date).ToLongTimeString())]" -AsJob
-Get-Job | Wait-Job
 
 $vm_arr = Get-VM -Name $VM_MASK
+
+# take snapshots
+$vm_arr | Checkpoint-VM -SnapshotName "$VM_SNAPSHOT_NAME_PREFIX [$((Get-Date).ToShortDateString()) $((Get-Date).ToLongTimeString())]" -AsJob
+Get-Job | Wait-Job
+
 
 foreach($vm in $vm_arr)
 {
@@ -23,11 +22,20 @@ foreach($vm in $vm_arr)
   }
 }
 
-# start 
-#Get-VM $VM_MASK | Start-VM -AsJob
-#Get-Job | Wait-Job
 
 # remove completed jobs
 Get-Job -State "Completed" | Remove-Job
+
+# send info to telegram
+$jobs = Get-Job
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+if ($jobs.Length -gt 0) { 
+    Invoke-WebRequest -URI "http://api.telegram.org/bot$($TELEGRAM_BOT_TOKEN)/sendMessage?chat_id=$($TELEGRAM_CHAT_ID)&text=Checkpoints `"$($VM_SNAPSHOT_NAME_PREFIX)`" of the `"$($VM_MASK)`" group are FAILED on $($env:COMPUTERNAME)"
+}
+else {
+    Invoke-WebRequest -URI "http://api.telegram.org/bot$($TELEGRAM_BOT_TOKEN)/sendMessage?chat_id=$($TELEGRAM_CHAT_ID)&disable_notification=true&text=Checkpoints `"$($VM_SNAPSHOT_NAME_PREFIX)`" of the `"$($VM_MASK)`" group are COMPLETED on $($env:COMPUTERNAME)"
+}
 
 exit
